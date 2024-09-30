eb8a629c2a5c9c680d758d5d06f740b05ef86170
kareemshono <kareem.shono.7@gmail.com>
2024-09-30 16:14:44 +0300

    initialization

---
+++ src/app/components/hero/Hero.jsx
@@ -0,0 +1,95 @@
+"use client"
+import Image from "next/image"
+import styles from "./Hero.module.scss"
+import { Inter } from "next/font/google"
+import { useEffect, useState } from "react"
+import { motion, AnimatePresence } from "framer-motion"
+
+const inter = Inter({ subsets: ["latin"] })
+
+const Hero = () => {
+  const [carouselImgs, setCarouselImgs] = useState([
+    { id: 0, img: "/girl-shopping.png" },
+    { id: 1, img: "/girl-shopping2.png" },
+    { id: 2, img: "/girl-shopping4.jpg" },
+    { id: 3, img: "/girl-shopping3.png" },
+  ])
+
+  const [currentImage, setCurrentImage] = useState(0)
+  const [direction, setDirection] = useState(1) // To track slide direction
+
+  // Auto switch images every 2 seconds
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDirection(1) // slide to the right
+      setCurrentImage((prevImage) =>
+        prevImage === carouselImgs.length - 1 ? 0 : prevImage + 1
+      )
+    }, 4000)
+
+    return () => clearInterval(intervalId) // Cleanup interval on component unmount
+  }, [carouselImgs.length])
+
+  // Animation variants for image transitions
+  const imageVariants = {
+    initial: (direction) => ({
+      opacity: 0,
+      x: direction > 0 ? 100 : -100, // Slide in from the right if direction is positive, left if negative
+    }),
+    animate: {
+      opacity: 1,
+      x: 0, // Bring image to the center
+      transition: {
+        duration: 2,
+        ease: [0.22, 1, 0.36, 1],
+      },
+    },
+    exit: (direction) => ({
+      opacity: 0,
+      x: direction > 0 ? -100 : 100, // Slide out to the left if direction is positive, right if negative
+      transition: { duration: 1.5, ease: "easeInOut" },
+    }),
+  }
+
+  return (
+    <section className={styles.heroSection}>
+      <div className={styles.colLeft}>
+        <h1 className={styles.title}>For Women</h1>
+        <h3>Best</h3>
+        <p className={styles.subtitle}>
+          Discover the latest trends and timeless pieces, designed for <br /> the modern
+          woman. 
+          Shop our exclusive collection today.
+        </p>
+        <button className={`${styles.btn} ${inter.className}`}>Shop Now</button>
+      </div>
+
+      <div className={styles.colRight}>
+        <div className={styles.carouselContainer}>
+          <AnimatePresence custom={direction} mode="wait">
+            <motion.div
+              key={carouselImgs[currentImage].id}
+              className={styles.carouselSlide}
+              variants={imageVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              custom={direction} // Pass direction to variants
+            >
+              <Image
+                src={carouselImgs[currentImage].img}
+                width={600}
+                height={400}
+                alt={`carousel-image-${currentImage}`}
+                priority={true} // Preload images for better performance
+                style={{ objectFit: "cover", maxWidth: "100%", height: "auto" }}
+              />
+            </motion.div>
+          </AnimatePresence>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/app/components/navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import Link from "next/link"
+import styles from "./Navbar.module.scss"
+import Image from "next/image"
+const Navbar = () => {
+  return (
+<div className={styles.navContainer}>
+    <div className={styles.logo}>
+        <Link href="/">
+        <Image src={"/navLogo2.svg"} width={100} height={80} alt="logo"/>
+        </Link>
+    </div>
+    <nav className={styles.navbar}>
+        <button className={styles.hamBtn}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+        <ul className={styles.navList}>
+            <li className={styles.navItem}>
+                <Link href="/" className={styles.navLink}>
+                Home
+                </Link>
+            </li>
+            <li className={styles.navItem}>
+                <Link href="/" className={styles.navLink}>
+                Shop 
+                </Link>
+            </li>
+            <li className={styles.navItem}>
+                <Link href="/" className={styles.navLink}>
+                Featured 
+                </Link>
+            </li>
+            <li className={styles.navItem}>
+                <Link href="/" className={styles.navLink}>
+                About
+                </Link>
+            </li>
+            <li className={styles.navItem}>
+                <Link href="/" className={styles.navLink}>
+                Contact Us
+                </Link>
+            </li>
+            <li className={styles.navItem}>
+                <Link href="/" className={styles.navLink}>
+                Sign In
+                </Link>
+            </li>
+            <li className={styles.navItem}>
+                <Link href="/" className={styles.navLink}>
+                <Image src={"/icons/cart.svg"} width={30} height={30} alt="cart vector" />
+                </Link>
+            </li>
+        </ul>
+    </nav>
+</div>
+  )
+}
+
+export default Navbar
